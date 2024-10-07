d5d06b1814c17e25319131081b7e9cf1a62314e9
Parameswari Bala <parameswaribala@gmail.com>
2024-10-07 10:19:04 +0530

    operators added

--- admin/admin.py
+++ admin/admin.py
@@ -44,3 +44,18 @@ print(date.today().strftime("%d/%m/%Y"));
 ipaddress=copyaddress='178.8.8.91';
 print(ipaddress,'=>',copyaddress);
 
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  3 12:42:27 2018
+
+@author: Balasubramaniam
+"""
+
+
+basevalue=12;
+score=1112
+print(score/basevalue);
+#floor operator
+print(score//basevalue);
+#exponent operator
+print(basevalue**2);
