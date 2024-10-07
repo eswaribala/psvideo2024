b018db674416ffdd0de90569ec931cacf5fea0bb
Parameswari Bala <parameswaribala@gmail.com>
2024-10-07 10:17:17 +0530

    identidier demo

--- admin/admin.py
+++ admin/admin.py
@@ -14,9 +14,33 @@ for i in range(1,10):
     
     
     
+ 
     
     
     
     
     
-    
+#type conversions
+
+#hex form conversion
+memoryAddress=255;
+hexData=hex(memoryAddress)
+print(hexData);
+print("Memory Address in hex form %X" %(memoryAddress));
+#complex Number
+real=56;
+imaginary=45;
+print(complex(real,imaginary));
+#binary to decimal
+print(int('10010',2));
+#decimal to binary
+print(bin(56));
+
+from datetime import date;
+#format the date
+print(date.today().strftime("%d/%m/%Y"));
+
+#multiple assignments
+ipaddress=copyaddress='178.8.8.91';
+print(ipaddress,'=>',copyaddress);
+
